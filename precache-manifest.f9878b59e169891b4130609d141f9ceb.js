e6ba48a427f8bfb60d3905555e553a43466064c5
chunfang.yao <chunfang.yao@highsoft.ltd>
2020-04-18 15:03:10 +0800

    Updates

---
+++ precache-manifest.f9878b59e169891b4130609d141f9ceb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fe1fe9fd13941805b359ff3c1b8b733",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b55fda9625c48b2692d",
+    "url": "/static/css/main.e58276a6.chunk.css"
+  },
+  {
+    "revision": "34d64e6fc59c9870f2b3",
+    "url": "/static/js/2.961ed56c.chunk.js"
+  },
+  {
+    "revision": "2ca78df4e200ab631a9acbb7a85ee5b8",
+    "url": "/static/js/2.961ed56c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b55fda9625c48b2692d",
+    "url": "/static/js/main.0d8b6405.chunk.js"
+  },
+  {
+    "revision": "471386392d6e8a081e55",
+    "url": "/static/js/runtime-main.968dbaa3.js"
+  },
+  {
+    "revision": "a836ebea712ebefe5f4b8db3ab0e27aa",
+    "url": "/static/media/apartments.a836ebea.png"
+  },
+  {
+    "revision": "9c075d360687127b5f1b83ebd81b5961",
+    "url": "/static/media/background.9c075d36.png"
+  },
+  {
+    "revision": "f36488f9d03af154a9410f7dffaf7c63",
+    "url": "/static/media/chu.f36488f9.png"
+  },
+  {
+    "revision": "7aae83d4fe5c9b425755e3c96521144e",
+    "url": "/static/media/fang.7aae83d4.png"
+  },
+  {
+    "revision": "591fd7d27c2aa3b7c0455513dfa5d1b2",
+    "url": "/static/media/jing.591fd7d2.png"
+  },
+  {
+    "revision": "c3b9d3e8abb8b309f5d4f1a96c589c5a",
+    "url": "/static/media/server.c3b9d3e8.png"
+  },
+  {
+    "revision": "d49ec769cff686e1804c726f6a974a13",
+    "url": "/static/media/share.d49ec769.png"
+  },
+  {
+    "revision": "123e26b8ee507b746dc9ee073074a20b",
+    "url": "/static/media/short.123e26b8.png"
+  },
+  {
+    "revision": "028367260f52068cbee8753d0b0f5917",
+    "url": "/static/media/whole.02836726.png"
+  },
+  {
+    "revision": "53c6b7c226a1b0601c4113df703512e1",
+    "url": "/static/media/xu.53c6b7c2.png"
+  },
+  {
+    "revision": "7ac7e56bfebab4dfc65a5d797c687e86",
+    "url": "/static/media/you.7ac7e56b.png"
+  },
+  {
+    "revision": "3f6dd6e37c550be33c1eef8f1ee7c129",
+    "url": "/static/media/zheng.3f6dd6e3.png"
+  }
+]);
